b3da300d14752e9ed075268dd8671fac1984bcf2
xabdulla <abdullashaker002@gmail.com>
2025-08-07 15:53:49 +0300

    fixed services

--- react-hoot-front-end/src/services/hootService.js
+++ react-hoot-front-end/src/services/hootService.js
@@ -2,7 +2,40 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`
 
 const index = async () => {
   try {
-    console.log('inside hoot index function')
+    const res = await fetch(BASE_URL)
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const show = async (hootId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${hootId}`)
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const create = async (formData) => {
+  try {
+    const token = localStorage.getItem('token')
+
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(formData)
+    })
+
+    const data = await res.json()
+    return data
+
   } catch (err) {
     console.log(err)
   }
@@ -10,4 +43,6 @@ const index = async () => {
 
 export {
   index,
+  show,
+  create,
 }
